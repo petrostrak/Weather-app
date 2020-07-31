a768a01785a46b76fe0f5b03224a10e69ee90257
petros <pit.trak@gmail.com>
2020-07-31 19:36:52 +0300

    added json parameter to true

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const request = require('request')
 
-// const url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=fb6bf32fca85aaac663b3c4dc005c1e9' 
-const url = 'https://api.darksky.net/forecast/9d1465c6f3bb7a6c71944bdd8548d026/37.8267,-122.4233'
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Athens,gr&appid=fb6bf32fca85aaac663b3c4dc005c1e9' 
 
-request({ url: url}, (error, response)=>{
-    const data = JSON.parse(response.body)
-    console.log(data);
+request({ url: url, json: true}, (error, response)=>{
+    console.log(response);
+    // const data = JSON.parse(response.body)
+    // console.log(data);
 })
 
