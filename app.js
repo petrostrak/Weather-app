0f85c36862882d43fc61366dc06628f9afbf96a7
petros <pit.trak@gmail.com>
2020-08-01 09:16:38 +0300

    refactored geocode to utils folder and export it to app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
 // const url = 'https://api.openweathermap.org/data/2.5/weather?q=Athens,gr&appid=fb6bf32fca85aaac663b3c4dc005c1e9' 
 
@@ -8,16 +9,22 @@ const request = require('request')
 //     // console.log(data);
 // })
 
-const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGl0dHJhayIsImEiOiJja2RhaG5xajEwbmt6MnptaXp4enMydjJpIn0.E5-EUQVMQWA5_X4zyoL0kQ&limit=1'
+// const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGl0dHJhayIsImEiOiJja2RhaG5xajEwbmt6MnptaXp4enMydjJpIn0.E5-EUQVMQWA5_X4zyoL0kQ&limit=1'
 
-request({ url:url, json: true }, (error, response) => {
-    if(error){
-        console.log('Unable to connect to Geocoding');
-    } else if (response.body.features.length === 0){
-        console.log('Unable to find location');
-    } else {
-        const latitude = response.body.features[0].center[0]
-        const longitude = response.body.features[0].center[1]
-        console.log(latitude, longitude);
-    }
+// request({ url:url, json: true }, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to Geocoding');
+//     } else if (response.body.features.length === 0){
+//         console.log('Unable to find location');
+//     } else {
+//         const latitude = response.body.features[0].center[0]
+//         const longitude = response.body.features[0].center[1]
+//         console.log(latitude, longitude);
+//     }
+// })
+
+
+geocode('Tokyo', (error, data) => {
+    console.log('error', error);
+    console.log('data', data);
 })
